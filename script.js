c509125b15840b5434323e660ebe2a9942894d96
@fuad2011 <M.Fuad.f2010@gmail.com>
2025-02-15 11:19:42 +0400

    7i

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log(screen.availHeight)
 console.log(screen.availWidth)
 function func1() {
-myWindow = window.open("", "", "width=200, height=100");
+myWindow = window.open("", "", "width=200, height=200");
 }  
 function func2() {
 myWindow.resizeTo(300, 300);
